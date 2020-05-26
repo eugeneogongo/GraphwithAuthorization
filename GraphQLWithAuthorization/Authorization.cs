63421558a852c27822c1578a6d090745189c6e7e
Neptune <eugeneogongo@live.com>
2020-05-26 12:26:14 +0300

    Add authenitication and authorization

---
+++ GraphQLWithAuthorization/Authorization.cs
@@ -0,0 +1,20 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace GraphQLWithAuthorization
+{
+    public static class Authorization
+    {
+        public static bool CheckClaims(this ResolveFieldContext<Object> context,string claims)
+        {
+            var user = context.UserContext as ClaimsPrincipal;
+
+            return user.IsInRole(claims);
+        }
+
+    }
+}
---
+++ GraphQLWithAuthorization/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQLWithAuthorization.JWT;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace GraphQLWithAuthorization.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : Controller
+    {
+        private readonly IConfiguration config;
+
+        public LoginController(IConfiguration configuration)
+        {
+            this.config = configuration;
+        }
+        public IActionResult Index()
+        {
+            var jwt = new JWTGenerator(config);
+            return Content(jwt.GenerateSecurityToken("me@gmail.com"));
+
+        }
+    }
+}
